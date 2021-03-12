0d959a7143da0be0704a398c666c28ab0cebaa7a
Guiller A. Caserotto <gcaserotto@tsjbaires.gov.ar>
2021-03-12 16:19:02 -0300

    initial import

---
+++ app/index.js
@@ -0,0 +1,30 @@
+
+"use strict";
+
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+
+  res.status(200).send("holis world");
+
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
+
+
+module.exports = app;
