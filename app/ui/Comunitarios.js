a00cf68ce27f4eee2e21b0acd1c6b75148952255
Guillermo Caserotto <gcaserotto@tsjbaires.gov.ar>
2021-04-03 18:17:57 -0300

    comunitarios con hook

--- app/ui/Comunitarios.js
+++ app/ui/Comunitarios.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
-//import hookActions from "../actions/hookActions";
-//import { comunitariosReducer } from "../reducers/hooksReducers";
+import hookActions from "../actions/hookActions";
+import { comunitariosReducer } from "../reducers/hooksReducers";
 
 const Comunitarios = function Comunitarios() {
   
@@ -14,7 +14,7 @@ const Comunitarios = function Comunitarios() {
   }
 
   React.useEffect(() => {
-    //hookActions.getComunitarios(setComunitarios);
+    hookActions.getComunitarios(setComunitarios);
   },[]);
 
   const handleRowClick = function(id){
