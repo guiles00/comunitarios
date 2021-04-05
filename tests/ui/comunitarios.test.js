b8579087e5b4eb0fb3d0da8ccc05c0544404eeb6
Guillermo Caserotto <gcaserotto@tsjbaires.gov.ar>
2021-04-05 18:37:26 -0300

    no modifique nada

--- tests/ui/comunitarios.test.js
+++ tests/ui/comunitarios.test.js
@@ -4,17 +4,32 @@ import {BrowserRouter as Router } from "react-router-dom";
 import Comunitarios from "../../app/ui/Comunitarios";
 import fixtureComunitarios from "../fixture/comunitarios";
 
-test("",()=>{})
-// describe("Comunitario",()=>{
-  
-//     const setup = ()=>{
-//         return shallow(
-//            <Comunitarios />
-//            );     
-//     }
+import { storeFactory }  from "../helpers/testUtils";
 
-//     test("It Renders ok",() =>{
-//       const wrapper = setup();
-//       expect(wrapper.find("#comunitarios-list").length).toBe(1)
-//     });
-// });
+const initialState = {
+  isLoading: false,
+  error:"",
+  comunitarios: []
+}
+
+describe("Comunitario",()=>{
+
+    const setup = (initialState)=>{
+
+      const store = storeFactory(initialState);
+   
+      return mount(
+          <Provider store={store}>
+            <Router>
+
+            <Comunitarios comunitarios={[]}/>
+            </Router>
+          </Provider>
+           );
+    }
+
+    test("It Renders ok",() =>{
+      const wrapper = setup();
+      expect(wrapper.find("#comunitarios-list").length).toBe(1)
+    });
+});
