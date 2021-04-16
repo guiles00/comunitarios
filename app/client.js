62c5def8378d885a41fad182f1c5360a12d4f6a0
Guillermo Caserotto <gcaserotto@tsjbaires.gov.ar>
2021-04-16 19:16:55 -0300

    guardar fecha como String por un tema del redux store y persist

--- app/client.js
+++ app/client.js
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react'
 
 const { store, persistor } = configureStore();
-console.log(store)
+
 const jsx = (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
